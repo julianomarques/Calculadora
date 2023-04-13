60d44671e2cbdf95a94632f709fb01fdfe67effd
julianomarques <53973654+julianomarques@users.noreply.github.com>
2023-04-13 17:39:56 -0400

    mudança
    escritorioo

--- lib/main.dart
+++ lib/main.dart
@@ -12,12 +12,42 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  String numero = 'numero';
+  void calcular(String tecla) {
+    switch (tecla) {
+      case '0':
+      case '1':
+      case '2':
+      case '3':
+      case '4':
+      case '5':
+      case '6':
+      case '7':
+      case '8':
+      case '9':
+        setState(() {
+          numero += tecla;
+        });
+        break;
+
+      case 'AC':
+        setState(() {
+          numero = '0';
+        });
+
+        break;
+      default:
+        numero += tecla;
+        break;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Calculadora'),
+          title: const Center(child: Text('Calculadora')),
         ),
         body: Column(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
@@ -26,17 +56,20 @@ class _MyAppState extends State<MyApp> {
               mainAxisAlignment: MainAxisAlignment.end,
               children: [
                 Text(
-                  'Numero',
-                  style: TextStyle(fontSize: 48),
+                  numero,
+                  style: const TextStyle(fontSize: 48),
                 ),
               ],
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
-                Text(
-                  'AC',
-                  style: TextStyle(fontSize: 48),
+                GestureDetector(
+                  onTap: () => calcular('AC'),
+                  child: Text(
+                    'AC',
+                    style: const TextStyle(fontSize: 48),
+                  ),
                 ),
                 Text(''),
                 Text(''),
@@ -49,84 +82,134 @@ class _MyAppState extends State<MyApp> {
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
-                Text(
-                  '7',
-                  style: TextStyle(fontSize: 48),
+                GestureDetector(
+                  onTap: () {
+                    calcular('7');
+                  },
+                  child: Text(
+                    '7',
+                    style: TextStyle(fontSize: 48),
+                  ),
                 ),
-                Text(
-                  '8',
-                  style: TextStyle(fontSize: 48),
+                GestureDetector(
+                  onTap: () => calcular('8'),
+                  child: Text(
+                    '8',
+                    style: TextStyle(fontSize: 48),
+                  ),
                 ),
-                Text(
-                  '9',
-                  style: TextStyle(fontSize: 48),
+                GestureDetector(
+                  onTap: () => calcular('9'),
+                  child: Text(
+                    '9',
+                    style: TextStyle(fontSize: 48),
+                  ),
                 ),
-                Text(
-                  '/',
-                  style: TextStyle(fontSize: 48),
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Text(
-                  '4',
-                  style: TextStyle(fontSize: 48),
-                ),
-                Text(
-                  '5',
-                  style: TextStyle(fontSize: 48),
-                ),
-                Text(
-                  '6',
-                  style: TextStyle(fontSize: 48),
-                ),
-                Text(
-                  'X',
-                  style: TextStyle(fontSize: 48),
+                GestureDetector(
+                  onTap: () => calcular('/'),
+                  child: Text(
+                    '/',
+                    style: TextStyle(fontSize: 48),
+                  ),
                 ),
               ],
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
-                Text(
-                  '1',
-                  style: TextStyle(fontSize: 48),
+                GestureDetector(
+                  onTap: () => calcular('4'),
+                  child: Text(
+                    '4',
+                    style: TextStyle(fontSize: 48),
+                  ),
                 ),
-                Text(
-                  '2',
-                  style: TextStyle(fontSize: 48),
+                GestureDetector(
+                  onTap: () => calcular('5'),
+                  child: Text(
+                    '5',
+                    style: TextStyle(fontSize: 48),
+                  ),
                 ),
-                Text(
-                  '3',
-                  style: TextStyle(fontSize: 48),
+                GestureDetector(
+                  onTap: () => calcular('6'),
+                  child: Text(
+                    '6',
+                    style: TextStyle(fontSize: 48),
+                  ),
                 ),
-                Text(
-                  '-',
-                  style: TextStyle(fontSize: 48),
+                GestureDetector(
+                  onTap: () => calcular('X'),
+                  child: Text(
+                    'X',
+                    style: TextStyle(fontSize: 48),
+                  ),
                 ),
               ],
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
-                Text(
-                  '0',
-                  style: TextStyle(fontSize: 48),
+                GestureDetector(
+                  onTap: () => calcular('1'),
+                  child: Text(
+                    '1',
+                    style: TextStyle(fontSize: 48),
+                  ),
                 ),
-                Text(
-                  ',',
-                  style: TextStyle(fontSize: 48),
+                GestureDetector(
+                  onTap: () => calcular('2'),
+                  child: Text(
+                    '2',
+                    style: TextStyle(fontSize: 48),
+                  ),
                 ),
-                Text(
-                  '=',
-                  style: TextStyle(fontSize: 48),
+                GestureDetector(
+                  onTap: () => calcular('3'),
+                  child: Text(
+                    '3',
+                    style: TextStyle(fontSize: 48),
+                  ),
                 ),
-                Text(
-                  '+',
-                  style: TextStyle(fontSize: 48),
+                GestureDetector(
+                  onTap: () => calcular('-'),
+                  child: Text(
+                    '-',
+                    style: TextStyle(fontSize: 48),
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                GestureDetector(
+                  onTap: () => calcular('0'),
+                  child: Text(
+                    '0',
+                    style: TextStyle(fontSize: 48),
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () => calcular(','),
+                  child: Text(
+                    ',',
+                    style: TextStyle(fontSize: 48),
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () => calcular('='),
+                  child: Text(
+                    '=',
+                    style: TextStyle(fontSize: 48),
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () => calcular('+'),
+                  child: Text(
+                    '+',
+                    style: TextStyle(fontSize: 48),
+                  ),
                 ),
               ],
             ),
